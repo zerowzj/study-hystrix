12b013e7dcf82db1b5e9fef2bc926aa7a67f0591
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-06-18 09:51:43 +0800

    提交代码

--- src/main/java/study/springboot/hystrix/service/order/OrderService.java
+++ src/main/java/study/springboot/hystrix/service/order/OrderService.java
@@ -2,10 +2,13 @@ package study.springboot.hystrix.service.order;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service("orderService")
 public class OrderService {
 
@@ -15,8 +18,12 @@ public class OrderService {
             @HystrixProperty(name = "execution.isolation.thread.interruptOnTimeout", value = "true"),
             @HystrixProperty(name = "execution.isolation.thread.interruptOnCancel", value = "true"),
             @HystrixProperty(name = "execution.isolation.semaphore.maxConcurrentRequests", value = "100")})
-    public OrderInfo getOrderInfo() {
-        return null;
+    public OrderInfo getOrderInfo(String orderNo) {
+        log.info("{}", orderNo);
+
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.setOrderNo("6666666666");
+        return orderInfo;
     }
 
     @HystrixCommand(threadPoolProperties = {@HystrixProperty(name = "coreSize", value = "10"),
@@ -26,6 +33,8 @@ public class OrderService {
             @HystrixProperty(name = "queueSizeRejectionThreshold", value = ""),
             @HystrixProperty(name = "keepAliveTimeMinutes", value = "")})
     public List<OrderInfo> getOrderLt() {
-        return null;
+        log.info("");
+        List<OrderInfo> orderInfoLt = Lists.newArrayList();
+        return orderInfoLt;
     }
 }
